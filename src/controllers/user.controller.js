07c96ac84b219748c2fcff737d09cf40ed1e598d
niranjan-369 <niranjan534201@gmail.com>
2025-01-20 11:04:54 +0530

    Add channel profile and watch history retrieval features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from  "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
 import { sendMail } from "../utils/mailerservice.js"; // Import sendMail from mailService
-
+import { Subscription } from "../models/subscription.model.js";
 
 // hum use kagrenge access token ko baar baar isliye hum method bana rahe hai
 const generateAccessAndRefreshTokens = async(userId) => {
@@ -399,9 +399,130 @@ const getUSerChannelProfile = asyncHandler( async(req, res) => {
     if(!username?.trim()){
         throw new ApiError(400, "username is missing")
     }
+    
+    const channel = await User.aggregate([
+        // pipeline starts here stage 1
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "Subscription",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+
+            }
+        },
+        {
+            $lookup: {
+                from: "Subscription",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+
+            $addFields: {
+                subscribersCount:   {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
 
 
+
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "channel does not found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "channel profile fetched successfully here ")
+    )
 } )
+
+const getWatchHistory = asyncHandler( async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline:[
+                                {
+                                    $project: {
+                                        fullName:1,
+                                        username:1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "watched History fetched"
+        )
+    )
+})
 
 
 
@@ -413,5 +534,6 @@ export {
     changeCurrentPassword,
     getCurrentUser,
     resetUserPassword,
-    getUSerChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 };
